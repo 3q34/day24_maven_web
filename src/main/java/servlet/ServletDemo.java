61f0f4908af1e0742248ebaad8bb9d6251077924
cdx <08chendongxiao@sina.com>
2019-12-21 23:37:55 +0800

    maven_web第一个项目

---
+++ src/main/java/servlet/ServletDemo.java
@@ -0,0 +1,17 @@
+package servlet;
+
+import java.io.IOException;
+
+/**
+ * Created by cdx on 2019/12/21.
+ * desc:
+ */
+public class ServletDemo extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        request.getRequestDispatcher("/hello.jsp").forward(request, response);
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
